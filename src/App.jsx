1f15ff7fc50a4716cd80b7254112714e27e060d7
Nikhil7282 <nikhil.sa2020ece@sece.ac.in>
2023-09-26 23:02:59 +0530

    Password Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userDetails,setUserDetails]=useState({password:"",title:""})
 
+  const handleChange=(e)=>{
+    const {name,value}=e.target
+    setUserDetails({...userDetails,[name]:value})
+  }
+
+  const handleSubmit=()=>{
+    console.log(userDetails);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <div className='AddingPassword'>
+        <input type='text' placeholder='Password...' name='password' onChange={handleChange}/>
+        <input type='text' placeholder='Title...' name='title' onChange={handleChange}/>
+        <button onClick={handleSubmit}>Add Password</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
