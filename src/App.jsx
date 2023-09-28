8e0f07328aa5411578f7c815ac8631322e56aee7
Nikhil7282 <nikhil.sa2020ece@sece.ac.in>
2023-09-28 22:19:32 +0530

    Branch-Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+import toast,{Toaster} from 'react-hot-toast'
+export const url='http://localhost:8000'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userDetails,setUserDetails]=useState({password:"",title:""})
 
+  const handleChange=(e)=>{
+    const {name,value}=e.target
+    setUserDetails({...userDetails,[name]:value})
+  }
+
+  const handleSubmit=()=>{
+    axios.post(`${url}/passwords/addPassword`,userDetails)
+    .then((res)=>{
+      console.log(res.data);
+      toast.success('Password Added')
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <div className="AddingPassword">
+        <input type="text" name='password' placeholder='Password...' onChange={handleChange}/>
+        <input type="text" name='title' placeholder='Title..' onChange={handleChange}/>
+        <button onClick={handleSubmit}>Add Password</button>
+        <Toaster/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
