0cd3adc166fa459fac7cc332e36878b1451c59a6
Nikhil7282 <nikhil.sa2020ece@sece.ac.in>
2023-12-23 18:44:48 +0530

    auth

--- src/state/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore,createSlice} from "@reduxjs/toolkit"
-import LoginReducer from "./User/loginSlice"
-
-export const store=configureStore({
-    reducer:{
-      login:LoginReducer
-    },
-})
